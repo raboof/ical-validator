ae72b97f9d97b0cdc1227d5d0e8cc060ecce54cd
Arnout Engelen <arnouten@localhost.(none)>
2010-01-30 21:32:43 +0100

    keep a requestlog

--- src/main/java/net/bzzt/ical/WicketApplication.java
+++ src/main/java/net/bzzt/ical/WicketApplication.java
@@ -8,30 +8,30 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.wicket.IConverterLocator;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.settings.IRequestLoggerSettings;
 import org.apache.wicket.util.convert.ConversionException;
 import org.apache.wicket.util.convert.ConverterLocator;
 import org.apache.wicket.util.convert.IConverter;
 import org.wicketstuff.annotation.scan.AnnotatedMountScanner;
 
 /**
- * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * Application object for your web application. If you want to run this
+ * application without deploying, run the Start class.
  * 
  * @see net.bzzt.ical.Start#main(String[])
  */
-public class WicketApplication extends WebApplication
-{    
+public class WicketApplication extends WebApplication {
 	private static final Log LOG = LogFactory.getLog(WicketApplication.class);
-	
-    /**
-     * Constructor
-     */
-	public WicketApplication()
-	{
+
+	/**
+	 * Constructor
+	 */
+	public WicketApplication() {
 	}
-	
-	
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.apache.wicket.protocol.http.WebApplication#init()
 	 */
 	@Override
@@ -39,27 +39,32 @@ public class WicketApplication extends WebApplication
 		new AnnotatedMountScanner().scanPackage("net.bzzt.ical").mount(this);
 
 		LOG.info("Mode: " + getConfigurationType());
-		
+
+		// Get the logger
+		IRequestLoggerSettings reqLogger = getRequestLoggerSettings();
+
+		// Enable the logger
+		reqLogger.setRequestLoggerEnabled(true);
+
 		super.init();
 	}
-
-
 
 	/**
 	 * @see org.apache.wicket.Application#getHomePage()
 	 */
-	public Class<HomePage> getHomePage()
-	{
+	public Class<HomePage> getHomePage() {
 		return HomePage.class;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.apache.wicket.Application#newConverterLocator()
 	 */
 	@Override
 	protected IConverterLocator newConverterLocator() {
 		ConverterLocator locator = new ConverterLocator();
-		locator.set(URI.class, new IConverter(){
+		locator.set(URI.class, new IConverter() {
 
 			/**
 			 * 
@@ -76,16 +81,15 @@ public class WicketApplication extends WebApplication
 
 			public String convertToString(Object value, Locale locale) {
 				try {
-					return ((URI)value).getURI();
+					return ((URI) value).getURI();
 				} catch (URIException e) {
 					throw new ConversionException(e);
 				}
 			}
-			
+
 		});
-		
+
 		return locator;
 	}
 
-	
 }
